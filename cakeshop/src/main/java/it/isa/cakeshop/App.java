41ba49f44bfeed00615e07e9ec448fc1b1beb5ac
fatina84 <faten.ismail@gmail.com>
2025-10-18 19:31:18 +0200

    Fix character encoding for sysout

--- cakeshop/src/main/java/it/isa/cakeshop/App.java
+++ cakeshop/src/main/java/it/isa/cakeshop/App.java
@@ -4,10 +4,22 @@ import it.isa.cakeshop.cli.CommandHandler;
 import it.isa.cakeshop.io.IOConsole;
 import it.isa.cakeshop.io.IOProvider;
 import it.isa.cakeshop.service.CakeShop;
+import java.io.OutputStream;
+import java.io.PrintStream;
+import java.nio.charset.StandardCharsets;
 
 // Classe principale dell'applicazione
 public class App {
   public static void main(String[] args) {
+    try {
+      // Set the system output stream to use UTF-8
+      OutputStream out = System.out;
+      PrintStream printStream = new PrintStream(out, true, StandardCharsets.UTF_8.name());
+      System.setOut(printStream);
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+
     // Crea un nuovo negozio di torte
     CakeShop shop = new CakeShop();
 
